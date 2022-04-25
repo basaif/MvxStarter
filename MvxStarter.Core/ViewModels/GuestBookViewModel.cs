7eed7ba39cbf6bd20391d462c10d8f0807e99b6e
basaif <iman.basaif@gmail.com>
2022-04-25 23:40:54 +0300

    Add project files.

---
+++ MvxStarter.Core/ViewModels/GuestBookViewModel.cs
@@ -0,0 +1,74 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using MvxStarter.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvxStarter.Core.ViewModels
+{
+    public class GuestBookViewModel : MvxViewModel
+    {
+        private ObservableCollection<PersonModel> _people = new ();
+
+        public ObservableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people, value); }
+        }
+
+        private string _firstName = string.Empty;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set 
+            {
+                SetProperty(ref _firstName, value);
+                RaisePropertyChanged(() => FullName);
+                RaisePropertyChanged(() => CanAddGuest);
+            }
+        }
+        private string _lastName = string.Empty;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                SetProperty(ref _lastName, value);
+                RaisePropertyChanged(() => FullName);
+                RaisePropertyChanged(() => CanAddGuest);
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public void AddGuest()
+        {
+            PersonModel p = new()
+            {
+                FirstName = FirstName,
+                LastName = LastName
+            };
+
+            FirstName = string.Empty;
+            LastName = string.Empty;
+
+            People.Add(p);
+        }
+
+        public IMvxCommand AddGuestCommand{ get; set; }
+
+        public GuestBookViewModel()
+        {
+            AddGuestCommand = new MvxCommand(AddGuest);
+        }
+
+        public bool CanAddGuest => FirstName?.Length > 0 && LastName?.Length > 0;
+
+    }
+}
